c138ea3e490b95abc18d404e69331de7b779764c
Tobias Jaster <tobias.jaster@posteo.de>
2022-12-05 07:52:31 +0100

    Add 2022 Day 5

--- pkg/year2022/year2022_test.go
+++ pkg/year2022/year2022_test.go
@@ -75,3 +75,20 @@ func Benchmark2022Day04(b *testing.B) {
 		}
 	})
 }
+func Benchmark2022Day05(b *testing.B) {
+	Init()
+	input := aoc.TestInput(2022, 5)
+	p := aoc.NewPuzzle(2022, 5)
+	b.Run("PartA", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartA(input)
+		}
+	})
+	b.Run("PartB", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartB(input)
+		}
+	})
+}
