654faabb3cbcb6b87f5e7b790bfb2d5897489ee8
Tobias Jaster <tobias.jaster@posteo.de>
2022-12-10 07:57:43 +0100

    Solve 2022 Day 10

--- pkg/year2022/year2022.go
+++ pkg/year2022/year2022.go
@@ -7,14 +7,15 @@ import (
 
 func Init() {
 	aoc.Register(2022, map[int]aoc.Puzzle{
-		1: Day01{},
-		2: Day02{},
-		3: Day03{},
-		4: Day04{},
-		5: Day05{},
-		6: Day06{},
-		7: Day07{},
-		8: Day08{},
-		9: Day09{},
+		1:  Day01{},
+		2:  Day02{},
+		3:  Day03{},
+		4:  Day04{},
+		5:  Day05{},
+		6:  Day06{},
+		7:  Day07{},
+		8:  Day08{},
+		9:  Day09{},
+		10: Day10{},
 	})
 }
