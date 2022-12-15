1c58f6504231d5cea9f8b51a28a37dda8460d701
Tobias Jaster <tobias.jaster@posteo.de>
2022-12-15 14:23:43 +0100

    Solve 2022 Day 15

--- pkg/year2022/year2022_test.go
+++ pkg/year2022/year2022_test.go
@@ -245,3 +245,20 @@ func Benchmark2022Day14(b *testing.B) {
 		}
 	})
 }
+func Benchmark2022Day15(b *testing.B) {
+	Init()
+	input := aoc.TestInput(2022, 15)
+	p := aoc.NewPuzzle(2022, 15)
+	b.Run("PartA", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartA(input)
+		}
+	})
+	b.Run("PartB", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartB(input)
+		}
+	})
+}
