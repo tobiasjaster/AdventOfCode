cc3c525a2911f7e3e99c0d02dd59f612a27676ea
Tobias Jaster <tobias.jaster@posteo.de>
2023-12-03 17:32:17 +0100

    Add 2023 day 3

--- pkg/year2023/year2023_test.go
+++ pkg/year2023/year2023_test.go
@@ -41,3 +41,20 @@ func Benchmark2023Day02(b *testing.B) {
 		}
 	})
 }
+func Benchmark2023Day03(b *testing.B) {
+	Init()
+	input := aoc.TestInput(2023, 3)
+	p := aoc.NewPuzzle(2023, 3)
+	b.Run("PartA", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartA(input)
+		}
+	})
+	b.Run("PartB", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.PartB(input)
+		}
+	})
+}
